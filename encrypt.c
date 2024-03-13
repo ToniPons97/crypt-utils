f8a6e8ac86a347577b3f19ad0a2afb9932405e0d
Antonio Pons <toninopons@gmail.com>
2024-03-13 15:43:31 +0100

    refactoring.

--- encrypt.c
+++ encrypt.c
@@ -11,13 +11,17 @@ int main(int argc, char* argv[])
         char tar_compress_cmd[5000] = "";
         char compressed_name[500] = "";
         char rm_file_cmd[500] = "";
+        char rm_cmd_prefix[] = "rm -rf ";
+        char compressed_ext[] = ".tar.gz";
+        char encrypted_ext[] = ".gpg";
         char gpg_cmd[2000] = "";
         char gpg_output_name[500] = "";
 
-        strncpy(rm_file_cmd, "rm -rf ", sizeof("rm -rf ") + 1);
-        strncat(rm_file_cmd, filename, sizeof(filename) + 1);
-        strncpy(compressed_name, filename, strlen(filename) + 1);
-        strncat(compressed_name, ".tar.gz", strlen(".tar.gz") + 1);
+
+        strncpy(rm_file_cmd, rm_cmd_prefix, strlen(rm_cmd_prefix));
+        strncat(rm_file_cmd, filename, strlen(filename));
+        strncpy(compressed_name, filename, strlen(filename));
+        strncat(compressed_name, compressed_ext, strlen(compressed_ext));
         snprintf(tar_compress_cmd, 5000, "%s%s%s%s", "tar czf ", compressed_name, " ", filename);
 
         int status = system(tar_compress_cmd);
@@ -29,8 +33,8 @@ int main(int argc, char* argv[])
         
         system(rm_file_cmd);
 
-        strncpy(gpg_output_name, compressed_name, strlen(compressed_name) + 1);
-        strncat(gpg_output_name, ".gpg", strlen(".gpg") + 1);
+        strncpy(gpg_output_name, compressed_name, strlen(compressed_name));
+        strncat(gpg_output_name, encrypted_ext, strlen(encrypted_ext));
         snprintf(gpg_cmd, 2000, "%s%s%s%s", "gpg --output ", gpg_output_name, " --symmetric --no-symkey-cache ", compressed_name);
 
         status = system(gpg_cmd);
@@ -47,7 +51,7 @@ int main(int argc, char* argv[])
 
         if (option == 'y')
         {
-            strncat(rm_file_cmd, ".tar.gz", strlen(".tar.gz") + 1);
+            strncat(rm_file_cmd, compressed_ext, strlen(compressed_ext));
             system(rm_file_cmd);
         }
     } else {

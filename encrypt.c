f6c2e05431245ad87deeda9eb75c656741103406
Antonio Pons <toninopons@gmail.com>
2024-03-13 17:08:44 +0100

    Refactoring encrypt.c, decrypt.c is working.

--- encrypt.c
+++ encrypt.c
@@ -4,14 +4,13 @@
 
 int main(int argc, char* argv[])
 {
-    const char* filename = NULL;
-    filename = argv[1];
-
     if (argc != 2) {
         printf("[!] no data to encrypt.\n");
         return 1;
     }
 
+    const char* filename = NULL;
+    filename = argv[1];
     char tar_compress_cmd[5000] = "";
     char compressed_name[500] = "";
     char rm_file_cmd[500] = "";
@@ -21,13 +20,13 @@ int main(int argc, char* argv[])
     char encrypted_ext[] = ".gpg";
     char gpg_cmd[2000] = "";
     char gpg_output_name[500] = "";
+    char option;
 
     snprintf(rm_file_cmd, 500, "%s%s", rm_cmd_prefix, filename);
     snprintf(compressed_name, 500, "%s%s", filename, compressed_ext);
     snprintf(tar_compress_cmd, 5000, "%s%s%s%s", "tar czf ", compressed_name, " ", filename);
 
     int status = system(tar_compress_cmd);
-
     if (status == -1) {
         printf("Error executing command: %s\n", tar_compress_cmd);
         return 1;
@@ -39,13 +38,10 @@ int main(int argc, char* argv[])
     snprintf(gpg_cmd, 2000, "%s%s%s%s", "gpg --output ", gpg_output_name, " --symmetric --no-symkey-cache ", compressed_name);
 
     status = system(gpg_cmd);
-
     if (status == -1) {
         printf("Error executing command: %s\n", gpg_cmd);
         return 1;
     }
-
-    char option;
 
     printf("Do you wish to delete the unencrypted data? (y/n) ");
     scanf("%c", &option);

f11b97f26f652ac0b4102ccd42dc55c130bb7e03
Antonio Pons <toninopons@gmail.com>
2024-03-13 14:51:39 +0100

    First commit. Encryption is done. Decryption not so much :)

---
+++ decrypt.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/limits.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    
+}
---
+++ encrypt.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    const char *filename = NULL;
+    filename = argv[1];
+
+    if ((argc == 2))
+    {
+        char tar_compress_cmd[5000];
+        char compressed_name[500];
+        char rm_file_cmd[500];
+
+        strncpy(rm_file_cmd, "rm -rf ", sizeof("rm -rf ") + 1);
+        strncat(rm_file_cmd, filename, sizeof(filename) + 1);
+
+        strncpy(compressed_name, filename, strlen(filename) + 1);
+        strncat(compressed_name, ".tar.gz", strlen(".tar.gz") + 1);
+
+        snprintf(tar_compress_cmd, 5000, "%s%s%s%s", "tar czf ", compressed_name, " ", filename);
+
+        int status = system(tar_compress_cmd);
+
+        if (status == -1) {
+            printf("Error executing command: %s\n", tar_compress_cmd);
+            return 1;
+        }
+        
+        system(rm_file_cmd);
+
+        char gpg_cmd[2000];
+        char gpg_output_name[500];
+
+        strncpy(gpg_output_name, compressed_name, strlen(compressed_name) + 1);
+        strncat(gpg_output_name, ".gpg", strlen(".gpg") + 1);
+
+        snprintf(gpg_cmd, 2000, "%s%s%s%s", "gpg --output ", gpg_output_name, " --symmetric --no-symkey-cache ", compressed_name);
+
+        printf("Executing: %s\n", gpg_cmd);
+
+        status = system(gpg_cmd);
+
+        
+        if (status == -1) {
+            printf("Error executing command: %s\n", gpg_cmd);
+            return 1;
+        }
+
+        char option;
+
+        printf("Do you wish to delete the unencrypted file? (y/n) ");
+        scanf("%c", &option);
+
+        if (option == 'y')
+        {
+            strncat(rm_file_cmd, ".tar.gz", strlen(".tar.gz") + 1);
+            system(rm_file_cmd);
+        }
+    }
+    else
+    {
+        printf("[!] no file or directoy to encrypt.\n");
+    }
+
+    return 0;
+}

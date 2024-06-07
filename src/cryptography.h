f4449ff924b140103d1e12f304ed6e959649a28e
Antonio Pons <toninopons@gmail.com>
2024-06-07 10:28:50 +0200

    Resolved issue: Cancelling decryption process no longer results in file deletion.

--- src/cryptography.h
+++ src/cryptography.h
@@ -1,7 +1,10 @@
+#include <stdbool.h>
+
 #ifndef CRYPTOGRAPHY_H
 #define CRYPTOGRAPHY_H
 
 void encrypt(char* file_name);
 void decrypt(char* file_name);
+bool check_decryption_status(FILE* pipe);
 
 #endif
